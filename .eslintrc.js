5e20018f1a3894a4fb787371ea1e56f1bc3be3ed
AJ Lajom <arjhon.lajom@crayon.com>
2023-06-06 08:49:49 +0800

    eslint updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,11 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'prettier/@typescript-eslint',
-  ],
-  plugins: ['import'],
-  parserOptions: {
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
+  "env": {
+      "browser": true,
+      "es2021": true
   },
-  env: {
-    es6: true,
-    node: true,
+  "extends": "eslint:recommended",
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
   },
-  rules: {
-    'import/first': 'error',
-    'import/order': ['error', { 'newlines-between': 'always' }],
-  },
-};
+}
